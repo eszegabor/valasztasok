8162e260e02ecc24bbb9937ffcb52aa9dae94519
nitslaszlo <nitslaszlo@gmail.com>
2023-12-04 17:03:05 +0100

    VálasztásiEredmény osztály adattagok és konstruktor létrehozása

---
+++ src/VálasztásiEredmény.ts
@@ -0,0 +1,22 @@
+export default class VálasztásiEredmény {
+    // Láthatósági szintek:
+    // - public: alapértelmezés
+    // - private: #-karakter jelzi a tag azonosítója elején
+    // - protected: ki kell írni
+
+    // adattagok: (illik private láthatósági szinttel létrehozni őket)
+    #kerület: number;
+    #szavazatok: number;
+    #vezetékNév: string;
+    #keresztNév: string;
+    #pártJel: string;
+
+    constructor(sor: string) {
+        const m: string[] = sor.split(" ");
+        this.#kerület = parseInt(m[0]);
+        this.#szavazatok = parseInt(m[1]);
+        this.#vezetékNév = m[2];
+        this.#keresztNév = m[3];
+        this.#pártJel = m[4];
+    }
+}
